895124c1ceceb99736473a4d581b77a8ff6e1645
Ivan Poblette <A01652733@tec.mx>
2023-05-04 19:30:56 -0500

    fix identation in config file motor 3

--- microcontrolers/utils/pwmT/joint3_config.h
+++ microcontrolers/utils/pwmT/joint3_config.h
@@ -1,29 +1,29 @@
-#define JOINT3_ZERO_POSITION            60.5
+#define JOINT3_ZERO_POSITION               60.5
 
 /*Encoder resoluition in pulses per meter [pulses/m]*/
-#define JOINT3_ENCODER_RESOLUTION       50
+#define JOINT3_ENCODER_RESOLUTION          50
 
 /*Joint movement limit in terms of pulses 
 (Low limit is 0, since start rutine sets zero with limite switch)
 Arm movement preferable be within the HW limits*/ 
-#define JOINT3_LOW_LIMIT_HW                0   
-#define JOINT3_HIGH_LIMIT_HW               90  
+#define JOINT3_LOW_LIMIT_HW                 0   
+#define JOINT3_HIGH_LIMIT_HW                90  
 
 /**SW limits to restrict movement wtithin a given range*/
-#define JOINT3_LOW_LIMIT_SW                0   
-#define JOINT3_HIGH_LIMIT_SW               90  
+#define JOINT3_LOW_LIMIT_SW                 0   
+#define JOINT3_HIGH_LIMIT_SW                90  
 
 /*PID Control gains*/
-#define KP_3                            1
-#define KI_3                            1
-#define KD_3                            1
+#define KP_3                                1
+#define KI_3                                1
+#define KD_3                                1
 
 /*PINS Description*/
-#define POSITIVE_DIR_PIN_3                5
-#define NEGATIVE_DIR_PIN_3                5
-#define PWM_PIN_3                         18
-#define LIMIT_SWITCH_H_PIN_3              9
-#define LIMIT_SWITCH_L_PIN_3              9
+#define POSITIVE_DIR_PIN_3                  5
+#define NEGATIVE_DIR_PIN_3                  5
+#define PWM_PIN_3                           18
+#define LIMIT_SWITCH_H_PIN_3                9
+#define LIMIT_SWITCH_L_PIN_3                9
 
 /*PWM configuration*/
 #define PWM_CHANNEL_3                       4
